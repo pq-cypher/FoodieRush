8b94e9a51d37935b78fc41c80d079cb79e629fd7
Ezedgame28 <89473314+Ezedgame28@users.noreply.github.com>
2024-06-13 14:18:45 +0100

    Add files via upload

---
+++ js/food-api.js
@@ -0,0 +1,173 @@
+var food = [
+	{
+		id: 'food-1',
+		title: "White Rice And Vegetable Stew with plantain",
+		price: 1000,
+		crossedPrice: 1500,
+		discount: true,
+		pictureUrl: "food9.jpeg",
+		category: "nigerian-rice",
+		quantity: 0
+	},
+	{
+		id: 'food-2',
+		title: "White Rice And Tomato Stew",
+		price: 1000,
+		crossedPrice: 1500,
+		discount: true,
+		pictureUrl: "food10.jpeg",
+		category: "nigerian-rice",
+		quantity: 0
+	},
+	{
+		id: 'food-3',
+		title: "Fried Rice",
+		price: 1500,
+		crossedPrice: 0,
+		discount: false,
+		pictureUrl: "food11.jpeg",
+		category: "nigerian-rice",
+		quantity: 0
+	},
+	{
+		id: 'food-4',
+		title: "Jollof Rice",
+		price: 1500,
+		crossedPrice: 0,
+		discount: false,
+		pictureUrl: "food12.jpeg",
+		category: "nigerian-rice",
+		quantity: 0
+	},
+	{
+		id: 'food-5',
+		title: "Dal Makhani Bowl",
+		price: 1500,
+		crossedPrice: 0,
+		discount: false,
+		pictureUrl: "food5.jpg",
+		category: "rice-bowl",
+		quantity: 0
+	},
+	{
+		id: 'food-6',
+		title: "Chicken Bourguignon Bowl",
+		price: 1500,
+		crossedPrice: 0,
+		discount: false,
+		pictureUrl: "food6.jpg",
+		category: "rice-bowl",
+		quantity: 0
+	},
+	{
+		id: 'food-7',
+		title: "Mexican-Burrito Bowl",
+		price: 1500,
+		crossedPrice: 0,
+		discount: false,
+		pictureUrl: "food7.jpg",
+		category: "rice-bowl",
+		quantity: 0
+	},
+	{
+		id: 'food-8',
+		title: "Black Pepper Honey Chicken Bowl",
+		price: 1500,
+		crossedPrice: 0,
+		discount: false,
+		pictureUrl: "food8.jpg",
+		category: "rice-bowl",
+		quantity: 0
+	},
+	{
+	id: 'food-9',
+	title: "1 wrap of pounded yam",
+	price: 100,
+	crossedPrice: 150,
+	discount: true,
+	pictureUrl: "pounded-yam-wrap.jpg",
+	category: "swallow-wraps",
+	quantity: 0
+	},
+	{
+	id: 'food-10',
+	title: "1 wrap of Fufu/Akpu",
+	price: 100,
+	crossedPrice: 150,
+	discount: true,
+	pictureUrl: "fufu-akpu-wrap.jpg",
+	category: "swallow-wraps",
+	quantity: 0
+	},
+	{
+	id: 'food-11',
+	title: "1 wrap of semovita",
+	price: 100,
+	crossedPrice: 150,
+	discount: true,
+	pictureUrl: "semovita-wrap.jpg",
+	category: "swallow-wraps",
+	quantity: 0
+	},
+	{
+	id: 'food-12',
+	title: "1 wrap of Amala",
+	price: 100,
+	crossedPrice: 150,
+	discount: true,
+	pictureUrl: "amala-wrap.jpg",
+	category: "swallow-wraps",
+	quantity: 0
+	},
+	{
+	id: 'food-13',
+	title: "1 wrap of Eba",
+	price: 100,
+	crossedPrice: 150,
+	discount: true,
+	pictureUrl: "eba-wrap.jpg",
+	category: "swallow-wraps",
+	quantity: 0
+	},
+	{
+	id: 'food-14',
+	title: "Egusi soup",
+	price: 3000,
+	crossedPrice: 150,
+	discount: false,
+	pictureUrl: "egusi-soup.jpg",
+	category: "soup",
+	quantity: 0
+	},
+	{
+	id: 'food-15',
+	title: "Okro soup",
+	price: 3000,
+	crossedPrice: 150,
+	discount: false,
+	pictureUrl: "okro-soup.jpg",
+	category: "soup",
+	quantity: 0
+	},
+	{
+	id: 'food-16',
+	title: "1 plate of spaghetti",
+	price: 400,
+	crossedPrice: 0,
+	discount: false,
+	pictureUrl: "spaghetti.jpg",
+	category: "pasta",
+	quantity: 0
+	},
+	{
+	id: 'food-17',
+	title: "1 plate of macaroni",
+	price: 400,
+	crossedPrice: 0,
+	discount: false,
+	pictureUrl: "macaroni.jpg",
+	category: "pasta",
+	quantity: 0
+	}
+	
+];
